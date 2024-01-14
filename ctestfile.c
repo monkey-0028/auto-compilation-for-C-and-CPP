e99315c64b068691840b7c14d2bea3c8f1c174fa
satyaprakash <satyaprakashroy28@gmail.com>
2024-01-14 17:01:39 +0530

    made_code_for_0=1_and_0=0

---
+++ ctestfile.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+		printf("this is c test file\n");
+		printf("new line added \n");
+		printf("again new line added \n");
+		printf("this line is added by nano \n");	
+		return 0;
+}
